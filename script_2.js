4f851efdb36ab15e038f69da9ce3bc9b91b6ae3e
Nikita <neurenico@gmail.com>
2022-09-08 10:31:53 +0300

    some changed

--- script_2.js
+++ script_2.js
@@ -1,33 +1,33 @@
 //2) array filling
 
-const arr = [],
-    newArr = [];
+const arr2 = [],
+    newArr2 = [];
 
-function createArray() {
+function createArray2() {
 
     for (let i = 1; i <= 300; i++) {
-        arr.push(i);
+        arr2.push(i);
     }
 
-    console.log(arr);
+    console.log(arr2);
     console.log('');
 }
 
-createArray();
+createArray2();
 
 //filling array with even indexes
 
 function evenIndex() {
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr2.length; i++) {
         if (i % 2 == 0) {
-            newArr.push(arr[i]);
+            newArr2.push(arr2[i]);
         } 
     }
 
     console.log('Pulled even index values');
     console.log('--------------------------');
-    console.log(newArr);
+    console.log(newArr2);
 }
 
 evenIndex();
